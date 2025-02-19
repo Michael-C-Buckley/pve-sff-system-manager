6ec1a6ebffddd0ac075578738f9067cf76ce334d
Michael Buckley <michaelcbuckley@proton.me>
2025-02-19 18:35:26 -0500

    Corrected the forgotten .link for the systemd network

--- configs/systemd-links.nix
+++ configs/systemd-links.nix
@@ -1,7 +1,7 @@
 {config, ...}: let
   sff = config.custom.${config.custom.sff.current};
 in {
-  environment.etc."systemd/network/10-enx520p1".text = ''
+  environment.etc."systemd/network/10-enx520p1.link".text = ''
     [Match]
     MACAddress=${sff.enx520p1.macAddress}
 
@@ -10,7 +10,7 @@ in {
     MTUBytes=9000
   '';
 
-  environment.etc."systemd/network/12-enx520p2".text = ''
+  environment.etc."systemd/network/11-enx520p2.link".text = ''
     [Match]
     MACAddress=${sff.enx520p2.macAddress}
 
