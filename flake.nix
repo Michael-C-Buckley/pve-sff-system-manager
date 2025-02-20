14ee399d9f28ca85bab499909a1c5a6f3aaa1d78
Michael Buckley <michaelcbuckley@proton.me>
2025-02-20 06:56:34 -0500

    Created shortcuts for ansible for updating cluster members

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
   };
 
-  outputs = { system-manager, nix-devshells }: let
+  outputs = { nixpkgs, system-manager, nix-devshells, ... }: let
     sffMembers = [ "sff1" "sff2" "sff3" ];
     sffConfig = {host}: system-manager.lib.makeSystemConfig {
       modules = [
@@ -24,7 +24,21 @@
       ];
     };
   in {
-    devShells.x86_64-linux.default = nix-devshells.devShells.x86_64-linux.nixosServers;
+    devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
+      inherit (nix-devshells.devShells.x86_64-linux.nixosServers) buildInputs;
+      shellHook = let 
+        inv = "-i tools/ansible/inventory.ini";
+        playbook = "ansible-playbook ${inv} tools/ansible/";
+        rootcmd = "ansible sff_hosts_as_root ${inv} -m command -a ";
+      in ''
+        ${nix-devshells.devShells.x86_64-linux.nixos.shellHook or ""}
+        # Ansible Shortcuts
+        alias hms='${playbook}home-manager.yml'
+        alias sms='${playbook}system-manager.yml'
+        alias sffreboot='${rootcmd} "reboot"'
+        alias sffpoweroff='${rootcmd} "shutdown now"'
+      '';
+    };
     systemConfigs = builtins.listToAttrs (map (host: {
       name = host;
       value = sffConfig {inherit host;};
