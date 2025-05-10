17eef678a35073a1cbae9d6f91f97ceca2518158
Michael Buckley <michaelcbuckley@proton.me>
2025-05-10 15:27:00 -0400

    Updated flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable-small";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
 
     system-manager = {
       url = "github:numtide/system-manager";
@@ -26,13 +26,13 @@
   in {
     devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
       inherit (nix-devshells.devShells.x86_64-linux.nixosServers) buildInputs;
-      shellHook = let 
+      shellHook = let
         inv = "-i tools/ansible/inventory.ini";
         playbook = "ansible-playbook ${inv} tools/ansible/";
       in ''
         ${nix-devshells.devShells.x86_64-linux.nixos.shellHook or ""}
         # Ansible Shortcuts
-        alias sffh='${playbook}home-manager.yml'
+        alias sffh='ansible ${inv} -m command -a "home-manager switch --flake github:Michael-C-Buckley/nixos"
         alias sffs='${playbook}system-manager.yml'
         alias sffreboot='${playbook}reboot.yml'
         alias sffpoweroff='${playbook}shutdown.yml'
